fe4446711923a1ed6a44dc98d9a78b95e2e18702
Nadya Kanash <nadida.hi@gmail.com>
2020-10-20 13:17:31 +0300

    feat: solve Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if(str.length % 2 != 0) return false;
+
+    let resStr = str;
+
+    for(let i = 1; i <= str.length/2; i++) {
+        if(resStr.length == 0) break;
+        bracketsConfig.forEach(function(item) {
+            if(resStr.length == 0) return;
+            let target = item.join('');
+
+            while (true) {
+                let foundPos = resStr.indexOf(target);
+                if(foundPos == -1) break;
+                resStr = resStr.substring(0,foundPos) + resStr.substring(foundPos + 2,resStr.length);
+                if(resStr.length == 0) break;
+            }
+        });
+    }
+
+    return resStr.length > 0 ? false : true;
 }
